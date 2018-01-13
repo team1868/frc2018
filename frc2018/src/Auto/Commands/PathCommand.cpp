7eee0fb1e2561c5bbe267b9003fb2fc156014e86
Lili <lili.sun@gmail.com>
2018-01-12 20:40:16 -0800

    DriveController done, needs to be tested. Profiler done. Added framework for PathCommand and MotionProfile.

---
+++ frc2018/src/Auto/Commands/PathCommand.cpp
@@ -0,0 +1,11 @@
+#include <Auto/Commands/PathCommand.h>
+
+PathCommand::PathCommand() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PathCommand::~PathCommand() {
+	// TODO Auto-generated destructor stub
+}
+
