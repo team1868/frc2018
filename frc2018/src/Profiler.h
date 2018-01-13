4ebfc8221d1dd79ac8f3920b0eef796615570519
Lili <lili.sun@gmail.com>
2018-01-12 20:51:51 -0800

    Finished DriveController and Profiler. Need to be tested.

--- frc2018/src/Profiler.h
+++ frc2018/src/Profiler.h
@@ -1,10 +1,18 @@
+#include "RobotModel.h"
+#include <string.h>
+
 #ifndef SRC_PROFILER_H_
 #define SRC_PROFILER_H_
 
 class Profiler {
 public:
-	Profiler();
+	Profiler(RobotModel* robot, std::string header);
+	double ReadTime();
 	virtual ~Profiler();
+private:
+	RobotModel* robot_;
+	double timeStart_;
+	std::string header_;
 };
 
 #endif /* SRC_PROFILER_H_ */
