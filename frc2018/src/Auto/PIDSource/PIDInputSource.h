8ea383b57383a244e03560a352b13557d31e009f
deepamarti <deepamarti@deepas-air.ndc.nasa.gov>
2018-01-14 15:46:29 -0800

    Added DriveStraightCommand
    Added PIDInput (navx and encoders) and PIDOutput (angle and distance)
    ~Deepa, Avery, Kaylia, and Divya

--- frc2018/src/Auto/PIDSource/PIDInputSource.h
+++ frc2018/src/Auto/PIDSource/PIDInputSource.h
@@ -1,10 +1,69 @@
 #ifndef SRC_AUTO_PIDINPUTSOURCE_H_
 #define SRC_AUTO_PIDINPUTSOURCE_H_
 
-class PIDInputSource {
+#include "RobotModel.h"
+#include "WPILib.h"
+
+
+ /*------------------- NAVX PID SOURCE!! -------------------*/
+
+class NavXPIDSource : public frc::PIDSource {
 public:
-	PIDInputSource();
-	virtual ~PIDInputSource();
+	/**
+	 * Assigns the robot and resets the accumulated yaw
+	 * @param RobotModel
+	 */
+	NavXPIDSource(RobotModel *robot);
+	/**
+	 * Calculates and returns AccumulatedYaw
+	 * @return AccumulatedYaw
+	 */
+	double PIDGet();
+	/**
+	 * Updates currYAW, calculates deltaYaw and accumulatedYaw
+	 */
+	double CalculateAccumulatedYaw();
+	/**
+	 * Sets AccumulatedYaw and deltaYaw to zero
+	 * Updates currYaw and lastYaw
+	 */
+	void ResetAccumulatedYaw();
+	/**
+	 * Destructor
+	 */
+	virtual ~NavXPIDSource();
+
+private:
+	double currYaw_, lastYaw_, deltaYaw_, accumulatedYaw_;
+	RobotModel *robot_;
+
+};
+
+/*------------------- TALON ENCODER PID SOURCE!! -------------------*/
+
+class TalonEncoderPIDSource : public frc::PIDSource {
+public:
+	/**
+	 * Assigns robot and sets averageTalonDistance to 0
+	 * @param RobotModel
+	 */
+	TalonEncoderPIDSource(RobotModel *robot);
+	/**
+	 *Gets distance from left and right encoders and sets averageTalonDistance
+	 *as average of the two
+	 *@return averageTalonDistance_
+	 */
+	double PIDGet();
+	/**
+	 * Destructor
+	 */
+	virtual ~TalonEncoderPIDSource();
+private:
+	RobotModel *robot_;
+	/**
+	 * Average distance of left and right encoders
+	 */
+	double averageTalonDistance_;
 };
 
 #endif /* SRC_AUTO_PIDINPUTSOURCE_H_ */
