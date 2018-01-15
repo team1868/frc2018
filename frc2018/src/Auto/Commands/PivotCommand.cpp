bf9fe388a6e00f870ba7855d4e93bd79d74981f7
Katie McLaughlin <kt.mclaughlin16@gmail.com>
2018-01-14 17:00:27 -0800

    Added PivotCommand
    Auto PivotCommand to turn to a desired angle; built but not tested
    Katie, Medha, Meichen, Divya, Elaine

---
+++ frc2018/src/Auto/Commands/PivotCommand.cpp
@@ -0,0 +1,134 @@
+#include "Auto/Commands/PivotCommand.h"
+
+PivotCommand::PivotCommand(RobotModel *robot, double desiredAngle, bool isAbsolutePosition, NavXPIDSource* navXSource) {
+
+	navXSource_ = navXSource;
+
+	initYaw_ = navXSource_->CalculateAccumulatedYaw();
+
+	if (isAbsolutePosition){
+		desiredDeltaAngle_ = CalculateDeltaAngle(desiredAngle);
+	} else {
+		desiredDeltaAngle_ = desiredAngle;
+	}
+
+	isDone_ = false;
+	robot_ = robot;
+	talonOutput_ = new PivotPIDTalonOutput(robot_);
+
+	pivotCommandStartTime_ = robot_->GetTime();
+	GetIniValues();
+	pivotPID_ = new PIDController(pFac_, iFac_, dFac_, navXSource_, talonOutput_);
+
+	numTimesOnTarget_ = 0;
+
+}
+
+void PivotCommand::GetIniValues() {
+
+	pFac_ = robot_->pini_->getf("PIVOT PID", "pFac", 0.0);
+	iFac_ = robot_->pini_->getf("PIVOT PID", "iFac", 0.0);
+	dFac_ = robot_->pini_->getf("PIVOT PID", "dFac", 0.0);
+	minDrivePivotOutput_ = robot_->pini_->getf("PIVOT PID", "minDrivePivotOutput", 0.0);
+	printf("PIVOT COMMAND p: %f, i: %f, d: %f\n", pFac_, iFac_, dFac_);
+}
+
+void PivotCommand::Init() {
+	//Profiler profiler(robot_, "Pivot Init");
+	initYaw_ = navXSource_->PIDGet();
+
+	pivotPID_->SetSetpoint(initYaw_ + desiredDeltaAngle_);
+	pivotPID_->SetContinuous(false);
+	pivotPID_->SetOutputRange(-0.8, 0.8);     //adjust for 2018
+	pivotPID_->SetAbsoluteTolerance(1.0);	 //adjust for 2018
+	pivotPID_->Enable();
+
+	SmartDashboard::PutNumber("Initial yaw", initYaw_);
+	printf("Initial NavX Angle: %f\n", initYaw_);
+	printf("Desired NavX Angle: %f\n", initYaw_ + desiredDeltaAngle_);
+
+	isDone_ = false;
+	numTimesOnTarget_ = 0;
+	pivotCommandStartTime_ = robot_->GetTime();
+}
+
+void PivotCommand::Reset() {
+	pivotPID_->Reset();
+	pivotPID_->Disable();
+	isDone_ = true;
+	free(pivotPID_);
+	printf("DONE FROM RESET \n");
+}
+
+void PivotCommand::Update(double currTimeSec, double deltaTimeSec) {
+
+	SmartDashboard::PutNumber("Pivot Error", pivotPID_->GetError());
+	SmartDashboard::PutNumber("Setpoint", pivotPID_->GetSetpoint());
+	SmartDashboard::PutNumber("Delta setpoint", pivotPID_->GetDeltaSetpoint());
+
+	double timeDiff = robot_->GetTime() - pivotCommandStartTime_;
+	bool timeOut = (timeDiff > 2.5);								//test this value
+
+	SmartDashboard::PutBoolean("Timed out", timeOut);
+	SmartDashboard::PutNumber("Pivot time diff", timeDiff);
+
+	if (pivotPID_->OnTarget()) {
+			numTimesOnTarget_++;
+		} else {
+			numTimesOnTarget_ = 0;
+		}
+		if ((pivotPID_->OnTarget() && numTimesOnTarget_ > 3) || timeOut) {
+			printf("Final NavX Angle: %f\n", navXSource_->PIDGet());
+			printf("Angle NavX Error %f\n", pivotPID_->GetAvgError());
+			pivotPID_->Reset();
+			pivotPID_->Disable();
+			isDone_ = true;
+			printf("PIVOT IS DONE \n");
+			if (timeOut) {
+				printf("FROM TIME OUT\n");
+			}
+		}
+		SmartDashboard::PutBoolean("Is done", isDone_);
+	}
+
+bool PivotCommand::IsDone() {
+	return isDone_;
+}
+
+double PivotCommand::CalculateDeltaAngle(double desiredAngle) {
+	double currYaw = fmod(initYaw_, 360.0);
+	return desiredAngle - currYaw;
+}
+
+PivotCommand::~PivotCommand() {
+	pivotPID_->Reset();
+	pivotPID_->Disable();
+	isDone_ = true;
+	free(pivotPID_);
+	printf("IS DONE FROM DECONSTRUCTOR\n");
+}
+
+PivotPIDTalonOutput::PivotPIDTalonOutput(RobotModel *robot){
+		robot_ = robot;
+		output_ = 0.0;
+}
+
+void PivotPIDTalonOutput::PIDWrite(double myOutput){
+	output_ = myOutput;
+
+	//robot_->SetDriveValues(RobotModel::kLeftWheels, -output_);
+	//robot_->SetDriveValues(RobotModel::kRightWheels, output_);
+	SmartDashboard::PutNumber("left output", -output_);
+	SmartDashboard::PutNumber("right output", output_);
+}
+
+double PivotPIDTalonOutput::GetOutput() {
+	return output_;
+}
+
+PivotPIDTalonOutput::~PivotPIDTalonOutput(){
+}
+
+
+
+
---
+++ frc2018/src/Auto/Commands/PivotCommand.h
@@ -0,0 +1,123 @@
+#ifndef SRC_AUTO_COMMANDS_PIVOTCOMMAND_H_
+#define SRC_AUTO_COMMANDS_PIVOTCOMMAND_H_
+
+#include "WPILib.h"
+#include <AHRS.h>
+#include "AutoCommand.h"
+#include "Auto/PIDSource/PIDInputSource.h"
+#include "Auto/PIDSource/PIDOutputSource.h"
+#include "RobotModel.h"
+#include "Profiler.h"
+
+/**
+ * PivotPIDTalonOutput a constructor Class for WPI PIDOutput for Pivoting
+ */
+class PivotPIDTalonOutput : public frc::PIDOutput {
+
+public:
+
+	/**
+	 * PivotPIDTalonOutput is a constructor that initializes robot_ and output_
+	 * @param robot a RobotModel
+	 */
+	PivotPIDTalonOutput(RobotModel *robot);
+
+	/**
+	 * PIDWrite a function that initializes output_ and sets the drive values
+	 */
+	void PIDWrite(double output);
+
+	/**
+	 * PivotPIDTalonOutput is a destructor
+	 */
+	virtual ~PivotPIDTalonOutput();
+
+	/**
+	 * @return output
+	 */
+	double GetOutput();
+
+private:
+	RobotModel *robot_;
+	double output_;
+};
+
+/**
+ * A class implementing Pivot PID the WPILibrary PID Controller
+ */
+class PivotCommand : public AutoCommand {
+
+public:
+	/**
+	 * PivotCommand a constructor
+	 * @param robot a RobotModel
+	 * @param desiredAngle a double that is the angle of the turn
+	 * @param isAbsolutePosition a bool that represents whether the angle is absolute position of deta angle
+	 * @param navXSource a NavXPIDSource
+	 */
+	PivotCommand(RobotModel *robot, double desiredAngle, bool isAbsolutePosition, NavXPIDSource* navXSource);
+
+	/**
+	 * PivotCommand a destructor
+	 */
+	virtual ~PivotCommand();
+
+	/**
+	 * gets Yaw from navX, sets Setpoint, continuous to false, output range, and absolute tolerance
+	 */
+	void Init();
+
+	/**
+	 * resets PID, sets isDone_ to true
+	 */
+	void Reset();
+
+	/**
+	 * gets PID values from ini file, sets to 0 if not present
+	 */
+	void GetIniValues();
+
+	/**
+	 * if PivotPID is on target more than three times then timeout
+	 * pivotPID resets, disable, isDone sets to true
+	 */
+	void Update(double currTimeSec, double deltaTimeSec);
+
+	/**
+	 * @return isDone_
+	 */
+	bool IsDone();
+
+private:
+	/**
+	 * calculates the angle needed to get to the desired angle
+	 * @param desiredAngle a double that is the angle of the turn
+	 */
+	double CalculateDeltaAngle(double desiredAngle);
+
+	double pFac_, iFac_, dFac_;
+	double desiredDeltaAngle_;
+	double initYaw_;
+
+	bool isDone_;
+
+	int numTimesOnTarget_;
+
+	RobotModel *robot_;
+	PIDController *pivotPID_;
+	NavXPIDSource * navXSource_;
+	PivotPIDTalonOutput *talonOutput_;
+
+	/**
+	 * Minimum output to correct for, less would be considered done
+	 */
+	double minDrivePivotOutput_;
+
+	double pivotCommandStartTime_;
+};
+
+
+
+
+
+#endif /* SRC_AUTO_COMMANDS_PIVOTCOMMAND_H_ */
